a9689cc30260c56dbcf37468e05f59c93175d69d
mic2tan <mic2tan@gmail.com>
2014-04-01 16:56:31 -0500

    Ch8 complete

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,11 +40,17 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+      #before do
+      #  fill_in "Name",         with: "Example User"
+      #  fill_in "Email",        with: "user@example.com"
+      #  fill_in "Password",     with: "foobar"
+      #  fill_in "Confirmation", with: "foobar"
+      #end
+      let(:user) { FactoryGirl.create(:user) }
+      before { valid_signup(user) }
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
       end
 
       describe "after saving the user" do
@@ -52,13 +58,9 @@ describe "User pages" do
         let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
-        it { should have_title(user.name) }
+        it { should have_title(User.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
+      end      
     end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,6 +7,12 @@ def valid_signup(user)
   fill_in "Confirmation", with: "foobar"
 end
 
+def valid_signin(user)
+  fill_in "Email",    with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
